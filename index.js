98e5c4549b565e832f5e55c40e9401cab72ffb5c
laissayuri <k.lais.sayuri@gmail.com>
2018-08-29 11:10:07 -0300

    Colher os dados do formulário

--- index.js
+++ index.js
@@ -6,14 +6,28 @@
 //usar nodemon index.js pra o servidor ja atualizar as mudanças sem ter que ficar inicializando toda hora
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const gatos = require("./data/gatos.json");
+
 
 const app = express();
 app.set("view engine", "ejs");
 app.use("/assets", express.static("static")); //css e imagens
+app.use(bodyParser.urlencoded());
 
 app.get("", (req, res) =>{ //request e response
     res.render("index"); // se for colocar com o html direto: res.send(`codigo do html`);
+});
+
+app.post("", (req, res) => {
+    let string = `
+        Nome: ${req.body.nome}\n Email: ${req.body.email} \n Mensagem: ${req.body.mensagem} \n
+    `;
+
+    fs.writeFile("mensagem.txt", string, {flag: "a"}, (err) =>{
+        res.render("obrigado");
+    });
 });
 
 app.get("/gatos", (req, res) =>{
