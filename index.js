6c68ecc9d0f6602cd63d825882f6f8c6e1996860
laissayuri <k.lais.sayuri@gmail.com>
2018-08-30 11:37:17 -0300

    Utilizar o Banco de Dados em vez de writeFile

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const expressMongoDb = require("express-mongo-db");
 const gatos = require("./data/gatos.json");
 
 
@@ -15,23 +16,29 @@ const app = express();
 app.set("view engine", "ejs");
 app.use("/assets", express.static("static")); //css e imagens
 app.use(bodyParser.urlencoded());
+app.use(expressMongoDb("mongodb://localhost/rent-a-cat"));
 
 app.get("", (req, res) =>{ //request e response
     res.render("index"); // se for colocar com o html direto: res.send(`codigo do html`);
 });
 
 app.post("", (req, res) => {
-    let string = `
-        Nome: ${req.body.nome}\n Email: ${req.body.email} \n Mensagem: ${req.body.mensagem} \n
-    `;
-
-    fs.writeFile("mensagem.txt", string, {flag: "a"}, (err) =>{
+    req.db.collection("mensagens").insert(req.body, (erro) => { //req.body pega as infos colocadas no input
+        console.log(erro);
         res.render("obrigado");
     });
+    
+    // let string = `
+    //     Nome: ${req.body.nome}\n Email: ${req.body.email} \n Mensagem: ${req.body.mensagem} \n
+    // `;
+
+    // fs.writeFile("mensagem.txt", string, {flag: "a"}, (err) =>{
+    //     res.render("obrigado");
+    // });
 });
 
 app.get("/gatos", (req, res) =>{
-    res.render("gatos", {"gatos":gatos}); //fala que tem que passar json no template
+    res.render("gatos", {"gatos":gatos}); //fala que tem que passar json no template ("template":json)
 });
 
 app.get("/sobre", (req, res) =>{
